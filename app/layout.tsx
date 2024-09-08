4c6250fcb59775f2690207d33efb56a5e3202335
Buğrahan Y. <78935299+bhy01dev@users.noreply.github.com>
2024-09-08 19:57:39 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,7 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'Bqrayvzdgn'
   },
   description: 'This is my portfolio.',
   openGraph: {
