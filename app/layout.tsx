2035accb6dac294920140151df41832c2839bd4a
Buğrahan Y. <78935299+bhy01dev@users.noreply.github.com>
2024-09-08 20:11:32 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'My Portfolio',
+    default: 'bqrayvzdgn',
     template: '',
   },
   description: 'This is my portfolio.',
