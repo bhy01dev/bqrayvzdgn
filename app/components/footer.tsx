1ea6f4683c1b8dd89f9a6edc7d7ebd584f35af71
Buğrahan Y. <78935299+bhy01dev@users.noreply.github.com>
2024-09-08 19:53:55 +0300

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -24,10 +24,10 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="/rss"
+            href="https://www.linkedin.com/in/bugrahan-yavuzdogan/"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">rss</p>
+            <p className="ml-2 h-7">linkedin</p>
           </a>
         </li>
         <li>
@@ -39,17 +39,6 @@ export default function Footer() {
           >
             <ArrowIcon />
             <p className="ml-2 h-7">github</p>
-          </a>
-        </li>
-        <li>
-          <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://vercel.com/templates/next.js/portfolio-starter-kit"
-          >
-            <ArrowIcon />
-            <p className="ml-2 h-7">view source</p>
           </a>
         </li>
       </ul>
